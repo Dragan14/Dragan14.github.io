1621f1adf416049477d9774d49c0b47b7227b4bf
dragans <dragans@student.unimelb.edu.au>
2022-07-25 20:12:52 +1000

    updated portfolio page

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+window.onscroll = function () {
+    const top = window.scrollY;
+    if (top >= 100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
 const footerText = "Dragan Stojanovski &copy; " + (new Date().getFullYear());
-$("footer > p").html(footerText);
+$("#footer > .container-fluid").html(footerText);
